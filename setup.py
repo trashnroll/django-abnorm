17a2b2b3069bf509ba7bf934e174f64765f78e3c
trashnroll <trashnroll@gmail.com>
2026-05-04 10:04:10 +0300

    Drop python 3.7 support

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
